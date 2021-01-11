13b89e1015ccdac8599db37ab3628b56bc3e4dfe
Thanh Long Do <thanhlongtld@gmail.com>
2021-01-11 16:16:45 +0700

    audio token

--- src/AudioPage.js
+++ src/AudioPage.js
@@ -3,32 +3,56 @@ import ReactAudioPlayer from "react-audio-player";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import 'react-jinke-music-player/assets/index.css'
 import axios from "axios";
-axios.defaults.withCredentials=true
+
+axios.defaults.withCredentials = true
 
 export default class AudioPage extends Component {
+    state = {
+        audioToken: ""
+    }
 
     componentDidMount() {
-        const accessToken=localStorage.getItem("accessToken")
-        if (!accessToken){
+        const accessToken = localStorage.getItem("accessToken")
+        if (!accessToken) {
             alert("Login Require")
             this.props.history.push("/login");
         }
         axios
-            .get(`http://localhost:8000/users/test_token`,{
-                headers:{
+            .get(`http://localhost:8000/users/test_token`, {
+                headers: {
                     'Authorization': `JWT ${accessToken}`,
-                    'Content-Type':"application/json",
+                    'Content-Type': "application/json",
                     withCredentials: true
                 }
             })
-            .then((res)=>{
-                if (res.data.newAccessToken){
-                    localStorage.setItem("accessToken",res.data.newAccessToken)
+            .then((res) => {
+                if (res.data.newAccessToken) {
+                    localStorage.setItem("accessToken", res.data.newAccessToken)
                 }
             })
-            .catch((err)=>{
+            .catch((err) => {
                 this.props.history.push("/logout")
                 console.log(err)
+            })
+        axios
+            .get(`http://localhost:8000/audio/gen_token/test.mp3`, {
+                headers: {
+                    'Authorization': `JWT ${accessToken}`,
+                    'Content-Type': "application/json",
+                    withCredentials: true
+                }
+            })
+            .then((res) => {
+                if (res.data.audioToken) {
+                    this.setState({
+                        audioToken: res.data.audioToken
+                    })
+                } else {
+                    alert("Cant find path")
+                }
+            })
+            .catch((err) => {
+                alert("Cant find path")
             })
     }
 
@@ -43,7 +67,7 @@ export default class AudioPage extends Component {
                         cover:
                             'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
                         musicSrc:
-                            "http://localhost:8000/audio",
+                            `http://localhost:8000/audio/${this.state.audioToken}`,
                         // support async fetch music src. eg.
                         // musicSrc: async () => {
                         //   return await fetch('/api')
